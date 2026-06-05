21daccdf22e73e9ca70f8382a15e48cbfb2d7e29
Madson Germano <madson.br@gmail.com>
2026-06-05 13:17:22 -0300

    fix: Ensure proper formatting and newline at end of apexcharts.js

--- src/boot/apexcharts.js
+++ src/boot/apexcharts.js
@@ -2,5 +2,5 @@ import { defineBoot } from '#q-app/wrappers'
 import VueApexCharts from 'vue3-apexcharts'
 
 export default defineBoot(({ app }) => {
-  app.use(VueApexCharts)
+	app.use(VueApexCharts)
 })
