5e9fcb7ac6a69e93abfdfdeff6027e5adf96632f
Madson Germano <madson.br@gmail.com>
2026-06-05 10:42:02 -0300

    feat(pwa): add service worker and manifest for PWA support
    - Added a new service worker file to manage caching and offline capabilities.
    - Created a manifest.json file to define app metadata and icons.
    - Introduced a safari pinned tab icon for better integration with Safari.
    - Implemented service worker registration logic to handle updates and offline scenarios.

--- quasar.config.js
+++ quasar.config.js
@@ -73,7 +73,7 @@ export default defineConfig((ctx) => {
           ssr: ctx.modeName === 'ssr',
 
           // you need to set i18n resource including paths !
-          include: [ fileURLToPath(new URL('./src/i18n', import.meta.url)) ]
+          include: [fileURLToPath(new URL('./src/i18n', import.meta.url))]
         }]
       ]
     },
@@ -81,6 +81,7 @@ export default defineConfig((ctx) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
     devServer: {
       // https: true,
+      port: 9173,
       open: true // opens browser window automatically
     },
 
@@ -122,7 +123,7 @@ export default defineConfig((ctx) => {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
@@ -174,7 +175,7 @@ export default defineConfig((ctx) => {
       // extendPackageJson (json) {},
 
       // Electron preload scripts (if any) from /src-electron, WITHOUT file extension
-      preloadScripts: [ 'electron-preload' ],
+      preloadScripts: ['electron-preload'],
 
       // specify the debugging port to use for the Electron app when running in development mode
       inspectPort: 5858,
